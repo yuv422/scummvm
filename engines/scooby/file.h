c3e39555ec66e5bfe9a8bb61910ad216b3db5df3
Eric Fry <yuv422@reversedgames.com>
2026-04-06 17:16:25 +1000

    SCOOBY: Initial work on intro sequence

---
+++ engines/scooby/file.h
@@ -0,0 +1,61 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
+#ifndef SCOOBY_FILE_H
+#define SCOOBY_FILE_H
+
+#include "common/file.h"
+#include "common/stream.h"
+
+namespace Scooby {
+
+class ScoobyEngine;
+
+class File : public Common::SeekableReadStream, public Common::ReadStreamEndian {
+public:
+	explicit File(ScoobyEngine *vm);
+	~File() override;
+
+	uint32 decompressBytes(uint32 offset, byte *dataPtr, uint32 dataSize);
+
+	bool eos() const override;
+
+	uint32 read(void *dataPtr, uint32 dataSize) override;
+
+	int64 pos() const override;
+
+	int64 size() const override;
+
+	bool seek(int64 offset, int whence) override;
+
+	bool offsetFromStart(int32 offset);
+private:
+	ScoobyEngine *_vm;
+	Common::File _fd;
+public:
+
+};
+
+
+} // End of namespace Scooby
+
+#endif //SCOOBY_FILE_H
---
+++ engines/scooby/offsets.h
@@ -0,0 +1,49 @@
+/* ScummVM - Graphic Adventure Engine
+ *
+ * ScummVM is the legal property of its developers, whose names
+ * are too numerous to list here. Please refer to the COPYRIGHT
+ * file distributed with this source distribution.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ */
+
+#ifndef SCOOBY_OFFSETS_H
+#define SCOOBY_OFFSETS_H
+
+enum Offsets {
+	data_initialVdpRegisters = 0x010123,
+	data_MainMenuBackgroundTilemap = 0x0165B8,
+	data_AcclaimLogoBgTilemap = 0x0219C0,
+	data_AcclaimLogoFgTilemap = 0x021B40,
+	data_AcclaimLogoTiles = 0x021E40,
+	data_AcclaimLogoPalette = 0x02241A,
+	data_SunSoftLogoTilemap = 0x02249A,
+	data_SunSoftLogoTiles = 0x02266E,
+	data_SunSoftLogoPalette = 0x0231A6,
+	data_SegaLogoTilemap = 0x023226,
+	data_SegaLogoTiles_packed = 0x023286,
+	data_SegaLogoPalette = 0x0236A0,
+	data_ScoobyLogoTiles = 0x023720,
+	data_ScoobyLogoTilemap = 0x0278E0,
+	data_ScoobyLogoPalette = 0x0281A0,
+	data_IllusionsLogoAnimTilemap = 0x028220,
+	data_IllusionsLogoFullTilemap = 0x02864C,
+	data_IllusionsLogoTiles = 0x0287C8,
+	data_IllusionsLogoPalette = 0x02967A
+};
+
+
+#endif //SCOOBY_OFFSETS_H
